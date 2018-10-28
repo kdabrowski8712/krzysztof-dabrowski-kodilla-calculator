a3b95ee1f21bdc0ff8d532e0e85a1f4357d20f1a
kdabrowski8712 <k.dabrowski8712@gmail.com>
2018-10-28 11:50:41 +0100

    Fist Commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+
+    public int subtractSecondMinusFirst(int A , int B) {
+        return B-A;
+    }
+
+    public int sum (int A , int B) {
+        return A+B;
+    }
+
+    public static void main (String [] args) {
+
+    }
+}
